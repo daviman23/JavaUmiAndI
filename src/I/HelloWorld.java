443ed85422db40f7517dd8a6e7848d41fab4bba5
Umi444 <Umi444@users.noreply.github.com>
2019-11-18 13:09:39 +0300

    rrrr123

--- src/I/HelloWorld.java
+++ src/I/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("rrrrrrr");
+        System.out.println("Umi ne kuda ne letit");
     }
 }
