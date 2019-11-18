2916930396829f55d21c15083abb9630c74f446f
daviman23 <56813088+daviman23@users.noreply.github.com>
2019-11-18 16:21:23 +0300

    фикс пакетов

--- src/Sergo/HelloWorld.java
+++ src/Sergo/HelloWorld.java
@@ -3,7 +3,5 @@ package Sergo;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("rrrrrrr");
-        System.out.println("Umi letit");
     }
 }
--- src/Sergo/Nuje.java
+++
@@ -1,7 +0,0 @@
-package Sergo;
-
-public class Nuje {
-    public static void main(String[] args) {
-        System.out.println("djn ");
-    }
-}
