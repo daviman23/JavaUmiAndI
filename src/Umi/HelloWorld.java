73fd8e29b72d4b1a99f1b736567de9388f1086e6
daviman23 <56813088+daviman23@users.noreply.github.com>
2019-11-23 21:59:23 +0300

    circel

---
+++ src/Umi/HelloWorld.java
@@ -0,0 +1,4 @@
+package Umi;
+
+public class HelloWorld {
+}
--- src/Umi/UmiNeLetit.java
+++
@@ -1,7 +0,0 @@
-package Umi;
-
-public class UmiNeLetit {
-    public static void main(String[] args) {
-        System.out.println("ura eto i");
-    }
-}
