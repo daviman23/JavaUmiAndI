94cebc6371bbbf6dfa904a7c5a9cc9f44ed92123
daviman23 <56813088+daviman23@users.noreply.github.com>
2019-12-16 20:04:02 +0300

    sudoku

---
+++ src/Sergo/fun/Sudoku/charwork.java
@@ -0,0 +1,18 @@
+package Sergo.fun.Sudoku;
+
+public class charwork {
+    public static void main(String[] args) {
+        char [] a = new char[3];
+        a[0] = '1';
+        a[2] = '1';
+        a[1] = '1';
+
+        a[1]++;
+        System.out.println(a[1]);
+        char o = '1';
+        while(o != '9'){
+            o++;
+
+        }
+        }
+    }
--- src/Sergo/fun/Sudoku/inTezt.java
+++ src/Sergo/fun/Sudoku/inTezt.java
@@ -4,40 +4,47 @@ import java.util.Scanner;
 
 public class inTezt {
     public static void main(String[] args) {
+        //ввод строк
         Scanner sudoku = new Scanner(System.in);
-
-        char [][] s = new char[9][9];
-       /* for (int i = 0; i < s.length ; i++) {
-            s[i] = sudoku.nextInt();
-            System.out.println(s[i]);
-        }*/
+        String []s = new String[9];
         for (int i = 0; i < s.length ; i++) {
-            System.out.println();
-            for (int j = 0; j < s.length ; j++) {
-                s[i][j] = '.';
-                System.out.print(s[i][j]);
+            s[i] = sudoku.next();
+        }
+//приоброзоывние в массив
+        char [][] c = new char[9][9];
+        for (int i = 0; i < 9 ; i++) {
+            String f = s[i];
+            for (int j = 0; j < 9; j++) {
+                c[j] = f.toCharArray();
             }
+            System.out.println();
+
+        }
+        for (int i = 0; i < 9 ; i++) {
+            int t = 0;
+            for (int j = 0; j < 9 ; j++) {
+                if(t <= 8){
+                    char a = c[i][j];
+                    char b = c[i][t];
+                    if(c[i][j] == c[i][t]) {
+                        c[i][j]++;
+                        System.out.print(c[i][j] + " ");
+                        t++;
+                    }
+                    else if(c[i][j] != c[i][t]) {
+                        t++;
+                    }
+                }
+
+            }
+            System.out.println();
         }
         System.out.println();
-        for (int i = 0; i < 9 ; i++) {
+        for (int i = 0; i < c.length; i++) {
+            for (int j = 0; j < c.length ; j++) {
+                System.out.print(c[i][j]);
+            }
             System.out.println();
-            for (int j = 0; j < 9 ; j++) {
-
-
-                if(s[i][j] > '0' && s[i][j] <= '9'){
-                    System.out.print(s[i][j]);
-                }
-                else if( s[i][j] == '.'){
-                    System.out.print(s[i][j]);
-                }
-                else
-                    return;
             }
         }
-
-    }
-}
-
-class test{
-
 }
