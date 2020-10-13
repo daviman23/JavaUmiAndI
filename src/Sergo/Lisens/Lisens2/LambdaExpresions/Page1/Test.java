bdda2cb32f6d121869be19131348abbddbc0b491
daviman23 <56813088+daviman23@users.noreply.github.com>
2020-10-13 14:46:33 +0300

    lessens12

--- src/Sergo/Lisens/Lisens2/LambdaExpresions/Page1/Test.java
+++ src/Sergo/Lisens/Lisens2/LambdaExpresions/Page1/Test.java
@@ -1,4 +1,23 @@
 package Sergo.Lisens.Lisens2.LambdaExpresions.Page1;
+interface Executable{
+    void execute();
+}
+
+class Runner {
+    public void run(Executable e){
+        e.execute();
+    }
+}
 
 public class Test {
+    public static void main(String[] args) {
+        Runner runner = new Runner();
+        runner.run(new Executable() {
+            @Override
+            public void execute() {
+                System.out.println("Hello");
+            }
+        });
+        runner.run(() ->System.out.println("Hello"));
+    }
 }
